088e2939f0478272370edb49f7517ff88118d323
Paul Chávez <paulchavezromero20@gmail.com>
2020-11-19 23:17:47 -0500

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header'
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hola Mundo</h1>
-		</div>
+		<>
+			<Header 
+				titulo='Cotizador de Prestamos'
+			/>
+		</>
 	);
 }
 
