4691ccf5df95b3057e9deaaafaad713acbc3567d
Paul Chávez <paulchavezromero20@gmail.com>
2020-11-19 23:39:04 -0500

    Formulario component added and create two states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header'
+import Formulario from './components/Formulario'
 
 function App() {
+
+    // Definir el states
+	const [ cantidad, guardarCantidad ] = useState(0),
+		  [ plazo, guardarPlazo ] = useState(0);
+
+
 	return (
 		<>
 			<Header 
 				titulo='Cotizador de Prestamos'
 			/>
+
+			<div className="container">
+				<Formulario 
+					cantidad={ cantidad }
+					plazo={ plazo }
+					guardarCantidad={ guardarCantidad }
+					guardarPlazo={ guardarPlazo }
+				/>
+			</div>
 		</>
 	);
 }
